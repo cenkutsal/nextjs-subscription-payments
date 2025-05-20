975e1efe801d3165456ed4590785a08ff184483e
cenk kutsal <cenkmazhar@gmail.com>
2025-05-20 15:58:46 +0300

    add supabase to the project and basic error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
 'use client';
 
 import React from 'react';
+import { Deposit, supabase } from '@/lib/supabase';
+import { useState } from 'react';
 
 export default function Page() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [errors, setErrors] = useState<string[]>([]);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const bankName = (form.elements[0] as HTMLInputElement).value;
-    const amount = (form.elements[1] as HTMLInputElement).value;
-    const dueDate = (form.elements[2] as HTMLInputElement).value;
-    console.log({ bankName, amount, dueDate }); // <— Verify this works!
+    setErrors([]);
+    const formData = new FormData(e.currentTarget);
+
+    const deposit: Deposit = {
+      bankName: formData.get('bankName') as string,
+      amount: Number(formData.get('amount')),
+      dueDate: formData.get('dueDate') as string
+    };
+
+    const errors: string[] = [];
+
+    if (!deposit.bankName) errors.push('Bank name is required');
+    if (isNaN(deposit.amount)) errors.push('Amount must be a number');
+    if (!deposit.amount) errors.push('Amount is required');
+    if (!deposit.dueDate) errors.push('Due date is required');
+
+    if (errors.length > 0) {
+      setErrors(errors);
+      return;
+    }
+
+    const { error } = await supabase.from('deposits').insert(deposit);
+    if (error) alert('Failed: ' + error.message);
+    else {
+      alert('Deposit added successfully');
+    }
   };
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -21,16 +45,19 @@ export default function Page() {
         className="flex flex-col items-center justify-center gap-4"
       >
         <input
+          name="bankName"
           className="border-2 border-gray-300 rounded-md p-2"
           type="text"
           placeholder="Bank Name"
         />
         <input
+          name="amount"
           className="border-2 border-gray-300 rounded-md p-2"
           type="number"
           placeholder="Amount"
         />
         <input
+          name="dueDate"
           className="border-2 border-gray-300 rounded-md p-2"
           type="date"
           placeholder="Due Date"
@@ -38,6 +65,13 @@ export default function Page() {
         <button className="bg-blue-500 text-white p-2 rounded-md" type="submit">
           Submit
         </button>
+        {errors.length > 0 && (
+          <div className="text-red-500">
+            {errors.map((error, index) => (
+              <p key={index}>{error}</p>
+            ))}
+          </div>
+        )}
       </form>
     </div>
   );
