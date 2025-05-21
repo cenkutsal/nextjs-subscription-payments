8bf6fba12cafe187b3683bd43f2c20c3c42b3889
cenk kutsal <cenkmazhar@gmail.com>
2025-05-21 21:47:10 +0300

    fix login redirect error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,12 +4,14 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const next = requestUrl.searchParams.get('next') || '/';
+  const redirectTo = requestUrl.searchParams.get('next') || '/';
 
   if (code) {
     const supabase = createClient();
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(requestUrl.origin + next);
+  return NextResponse.redirect(
+    new URL(redirectTo, process.env.NEXT_PUBLIC_SITE_URL)
+  );
 }
