13f0ea7024c40a7198eecdaa024d0019dd1e9b2a
cenk kutsal <cenkmazhar@gmail.com>
2025-05-22 20:29:38 +0300

    feat: enhance deposits page with back navigation and improve table styling

--- app/deposits/page.tsx
+++ app/deposits/page.tsx
@@ -1,7 +1,9 @@
 import DepositsTable from '@/components/DepositsTable';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
-import { Deposit } from '@/lib/supabase';
+import Button from '@/components/ui/Button';
+import Link from 'next/link';
+import { ArrowLeft } from 'lucide-react';
 
 export default async function Deposits() {
   const supabase = createClient();
@@ -26,16 +28,24 @@ export default async function Deposits() {
       `
     )
     .eq('user_id', user?.id)
-    .order('deposit_date', { ascending: false });
+    .order('due_date', { ascending: true });
 
   if (!deposits) {
     return <div>No deposits found</div>;
   }
 
   return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-2xl font-bold mb-4">Your Deposits</h1>
+    <div className="container mx-auto px-4 py-10">
+      <h1 className="text-2xl text-gray-200 font-bold mb-4 text-center">
+        Your Deposits
+      </h1>
       <DepositsTable deposits={deposits} />
+      <Link href="/" className="flex items-center justify-center ">
+        <Button className="flex md:w-1/3 gap-2 mt-4" variant="slim">
+          <ArrowLeft />
+          <span>Back to Home</span>
+        </Button>
+      </Link>
     </div>
   );
 }
--- components/DepositsTable.tsx
+++ components/DepositsTable.tsx
@@ -9,27 +9,27 @@ const DepositsTable = ({ deposits }: { deposits: Deposit[] | null }) => {
       <table className="min-w-full divide-y divide-gray-700">
         <thead>
           <tr>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Bank Name
             </th>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Alias
             </th>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Amount
             </th>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Deposit Date
             </th>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Due Date
             </th>
-            <th className="px-6 py-3 bg-gray-800 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
+            <th className="px-6 py-3 bg-blue-950 text-left text-xs font-medium text-gray-300 uppercase tracking-wider">
               Term Days
             </th>
           </tr>
         </thead>
-        <tbody className="bg-gray-900 divide-y divide-gray-700">
+        <tbody className="bg-gray-800/30 divide-y divide-gray-700">
           {deposits.map((deposit) => (
             <tr key={deposit?.id}>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
